b704e5286726d71d0cb7a105b0dbf620b1468096
Nicolas Lunet <nicolas.lunet@sap.com>
2022-09-20 09:59:27 +0200

    chore: adjust watch test

--- packages/fe-mockserver-core/test/unit/__testData/RootElement.js
+++ packages/fe-mockserver-core/test/unit/__testData/RootElement.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 module.exports = {
     getInitialDataSet(tenantId) {
         if (tenantId === 'tenant-003') {
             return require('./tenant-003/RootElement.json');
         }
-        return require('./RootElement.json');
+        return JSON.parse(fs.readFileSync(path.join(__dirname, 'RootElement.json')).toString('utf-8'));
     }
 };
