5612cc0846b764a84b42faa8f45f9937e2606389
Nicolas Lunet <nicolas.lunet@sap.com>
2023-10-23 14:30:25 +0200

    chore: rename file, fix eslint

--- packages/fe-mockserver-core/test/unit/data/fileBasedMockData.spec.ts
+++ packages/fe-mockserver-core/test/unit/data/fileBasedMockData.spec.ts
@@ -1,26 +1,21 @@
 import CDSMetadataProvider from '@sap-ux/fe-mockserver-plugin-cds';
 import { join } from 'path';
-import { ServiceConfig } from '../../../src';
-import { EntitySetInterface } from '../../../src/data/common';
+import type { ServiceConfig } from '../../../src';
 import { DataAccess } from '../../../src/data/dataAccess';
 import { ODataMetadata } from '../../../src/data/metadata';
 import { FileBasedMockData } from '../../../src/mockdata/fileBasedMockData';
 import FileSystemLoader from '../../../src/plugins/fileSystemLoader';
 let metadata!: ODataMetadata;
 const baseUrl = '/sap/fe/mock';
-describe('Function Based Mock Data', () => {
+describe('File Based Mock Data', () => {
     const fileLoader = new FileSystemLoader();
     const baseDir = join(__dirname, 'mockdata');
     const metadataProvider = new CDSMetadataProvider(fileLoader);
     let dataAccess: DataAccess;
-    let myEntitySet: EntitySetInterface;
-    let myOtherEntitySet: EntitySetInterface;
     beforeAll(async () => {
         const edmx = await metadataProvider.loadMetadata(join(baseDir, 'service.cds'));
         metadata = await ODataMetadata.parse(edmx, baseUrl + '/$metadata');
         dataAccess = new DataAccess({ mockdataPath: baseDir } as ServiceConfig, metadata, fileLoader);
-        myEntitySet = await dataAccess.getMockEntitySet('MyRootEntity');
-        myOtherEntitySet = await dataAccess.getMockEntitySet('MySecondEntity');
     });
     it('can generate data', () => {
         const mockData: any = [];
