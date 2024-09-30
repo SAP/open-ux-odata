990ebdda9a03d981da23c87705b5b72150a7a27f
Vincent ESCALIER <vincent.escalier@sap.com>
2024-09-30 15:28:18 +0200

    update snapshots

--- packages/fe-mockserver-core/test/unit/middleware.test.ts
+++ packages/fe-mockserver-core/test/unit/middleware.test.ts
@@ -127,7 +127,7 @@ describe('V4 Requestor', function () {
             {
               "@odata.context": "$metadata#RootElement(ID=1,IsActiveEntity=true)/_Elements",
               "@odata.count": 3,
-              "@odata.metadataEtag": "W/"62b2-j3ePZjiQElXD9LZZ+gEAOLMD5nc"",
+              "@odata.metadataEtag": "W/"63f6-6P/9UFLMfn2HIrza0TGMUWtfMPQ"",
               "value": [
                 {
                   "HasActiveEntity": true,
