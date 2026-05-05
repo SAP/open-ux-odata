a4176b94302bd6af40b3cd2c469f54bca36d515a
Andis Redmans <andis.redmans@sap.com>
2026-05-05 17:49:18 +0300

    fix: avoid crash
    avoid crash

--- packages/fe-mockserver-core/src/mockdata/fileBasedMockData.ts
+++ packages/fe-mockserver-core/src/mockdata/fileBasedMockData.ts
@@ -1304,7 +1304,7 @@ export class FileBasedMockData {
                 applyToActiveEntity
             );
             const sourceReference = this.getSourceReference(aggregationAnnotation);
-            const rootNodes = hierarchyNodes[''];
+            const rootNodes = hierarchyNodes[''] ?? [];
             rootNodes.forEach((rootNode: any) => {
                 this.buildTree(rootNode, hierarchyNodes, nodeProperty, 0, undefined);
             });
@@ -1475,7 +1475,7 @@ export class FileBasedMockData {
                 hierarchyDefinition,
                 applyToActiveEntity
             );
-            const rootNodes = hierarchyNodes[''];
+            const rootNodes = hierarchyNodes[''] ?? [];
             rootNodes.forEach((rootNode: any) => {
                 this.buildTree(rootNode, hierarchyNodes, nodeProperty, 0, undefined);
             });
